6f79e0a84f0ae59187776f2b6c5326d9d478811d
David Cramb <david.cramb@gmail.com>
2016-11-05 10:19:51 -0500

    12

--- BasicsOfForms/Controllers/AngularApiController.cs
+++ BasicsOfForms/Controllers/AngularApiController.cs
@@ -5,16 +5,18 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Mvc;
+using BasicsOfForms.Models;
+using BasicsOfForms.Controllers;
 
 namespace BasicsOfForms.Controllers
 {
     public class AngularApiController : ApiController
     {
-
+        [System.Web.Http.HttpPost]
         public void Post([FromBody]dynamic form)
         {
             int i = 0;
-            return form;
+             
 
         }
     }
--- BasicsOfForms/Scripts/app.js
+++ BasicsOfForms/Scripts/app.js
@@ -1,9 +1,21 @@
 ﻿var app = angular.module("BasicsOfForms", []);
 
-app.controller("BasicsCtrl", ["$scope", function($scope){
+app.controller("BasicsCtrl", ["$scope","$http", function($scope, $http){
 
-    $scope.formSubmit = () => {
-        console.log("hi!");
+    $scope.formSubmit = (event, user) => {
+        event.preventDefault();
+        var data = $("form").serialize();
+        $http.post("/api/AngularApi", data)
+        .then(
+        console.log(data));
     }
 
+    //$scope.formSubmitJq = function (event) {
+    //    $scope.user.email = $("#myform > input[type-email]").val();
+    //    $scope.user.password = $("#myform > input[type-password]").val();
+    //    $scope.user.passwordvalidation = $("#myform > input[type-passwordvalidation]").val();
+    //    $(http.post(
+    //        ))
+
+    //}
 }]);
